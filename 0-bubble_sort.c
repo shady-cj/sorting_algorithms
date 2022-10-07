6e7488e1ddeeb06dd2f59542529df8e9054bfb4c
shady-cj <petersp2000@gmail.com>
2022-10-07 17:21:10 +0100

    Betty coding style

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,6 +12,9 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, swapped = 0;
 	ssize_t placeholder;
+
+	if (array == NULL)
+		return;
 	for (i = 0; i < (size - 1); i++)
 	{
 		for (j = 0; j < (size - 1); j++)
