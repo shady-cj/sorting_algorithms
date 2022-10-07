95893b654ff660bd163ac48c73c38512f89b3ef0
shady-cj <petersp2000@gmail.com>
2022-10-07 17:01:47 +0100

    Added best case flag for bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,20 +10,22 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
+	size_t i, j, swapped = 0;
 	ssize_t placeholder;
-
 	for (i = 0; i < (size - 1); i++)
 	{
 		for (j = 0; j < (size - 1); j++)
 		{
 			if (array[j] > array[j + 1])
 			{
+				swapped = 1;
 				placeholder = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = placeholder;
 				print_array(array, size);
 			}
 		}
+		if (swapped == 0)
+			break;
 	}
 }
