42adf7cb1991a8b1e6f044478af8364ebc9e8cfc
shady-cj <petersp2000@gmail.com>
2022-10-10 08:28:41 +0100

    Adding checks if array is less than 2

--- sort.h
+++ sort.h
@@ -41,4 +41,6 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+k_gap_t *knuth_gap(size_t size);
+void free_gap(k_gap_t *head);
 #endif
