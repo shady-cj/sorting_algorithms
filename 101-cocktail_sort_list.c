f9d7d17f2efd9f10ac4db66069fa9f6bb1a43332
shady-cj <petersp2000@gmail.com>
2022-10-15 11:44:03 +0100

    bugfix: Fixing cocktail sort bugs

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,5 +1,56 @@
 #include "sort.h"
 
+
+/**
+ * swap_forward - This swaps the indexes forward as in normal
+ * bubble sort
+ * @current: The pointer to the current index in the iteration
+ * @head: The head of the doubly linked list
+ */
+
+void swap_forward(listint_t **current, listint_t **head)
+{
+	listint_t *pholder;
+
+	pholder = (*current)->next;
+	if ((*current)->prev)
+		((*current)->prev)->next = pholder;
+	else
+		*head = pholder;
+	if (pholder->next)
+		(pholder->next)->prev = *current;
+	pholder->prev = (*current)->prev;
+	(*current)->next = pholder->next;
+	(*current)->prev = pholder;
+	pholder->next = *current;
+	print_list(*head);
+}
+
+/**
+ * swap_backward - This swaps the indexes backward after initial
+ * bubble sort
+ * @current: The pointer to the current index in the iteration
+ * @head: The head of the doubly linked list
+ */
+void swap_backward(listint_t **current, listint_t **head)
+{
+	listint_t *pholder;
+
+	pholder = (*current)->prev;
+	if (pholder->prev)
+		(pholder->prev)->next = *current;
+	else
+		*head = *current;
+	((*current)->next)->prev = pholder;
+	pholder->next = (*current)->next;
+	(*current)->prev = pholder->prev;
+	(*current)->next = pholder;
+	pholder->prev = *current;
+	print_list(*head);
+}
+
+
+
 /**
  * cocktail_sort_list - This implement a cocktail
  * sort using doubly ljnked list
@@ -8,33 +59,30 @@
 
 void cocktail_sort_list(listint_t **list)
 {
-	listint_t *ptr, *shaker, *pholder;
+	listint_t *ptr, *shaker;
 	int sorted = 0;
+	size_t size = 0, i;
 
 	if (*list == NULL)
 		return;
 	ptr = *list;
+	while (ptr)
+	{
+		size++;
+		ptr = ptr->next;
+	}
+	if (size < 2)
+		return;
 	shaker = *list;
-	while (ptr->next)
+	for (i = 0; i < size; i++)
 	{
 		sorted = 0;
 		while (shaker->next)
 		{
-			pholder = shaker->next;
-			if (shaker->n > pholder->n)
+			if (shaker->n > shaker->next->n)
 			{
-				if (shaker->prev)
-					(shaker->prev)->next = pholder;
-				else
-					*list = pholder;
-				if (pholder->next)
-					(pholder->next)->prev = shaker;
-				pholder->prev = shaker->prev;
-				shaker->next = pholder->next;
-				shaker->prev = pholder;
-				pholder->next = shaker;
+				swap_forward(&shaker, list);
 				sorted = 1;
-				print_list(*list);
 				continue;
 			}
 			shaker = shaker->next;
@@ -44,23 +92,12 @@ void cocktail_sort_list(listint_t **list)
 		shaker = shaker->prev;
 		while (shaker->prev)
 		{
-			pholder = shaker->prev;
-			if (shaker->n < pholder->n)
+			if (shaker->n < shaker->prev->n)
 			{
-				if (pholder->prev)
-					(pholder->prev)->next = shaker;
-				else
-					*list = shaker;
-				(shaker->next)->prev = pholder;
-				pholder->next = shaker->next;
-				shaker->prev = pholder->prev;
-				shaker->next = pholder;
-				pholder->prev = shaker;
-				print_list(*list);
+				swap_backward(&shaker, list);
 				continue;
 			}
 			shaker = shaker->prev;
 		}
-		ptr = ptr->next;
 	}
 }
--- 101-main.c
+++ 101-main.c
@@ -39,7 +39,9 @@ listint_t *create_listint(const int *array, size_t size)
 int main(void)
 {
     listint_t *list;
-    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+    /*int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};*/
+    int array[] = {98, 95, 92, 91, 87, 84, 79, 68, 62, 47, 41, 39, 34, 32, 31, 22, 21, 20, 2, 1};
+    /* int array[] = {2, 1};*/
     size_t n = sizeof(array) / sizeof(array[0]);
 
     list = create_listint(array, n);
